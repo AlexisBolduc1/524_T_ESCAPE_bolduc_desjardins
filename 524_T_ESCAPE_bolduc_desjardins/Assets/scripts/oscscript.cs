9ca9e8107f4bb13d7884de892962174aab3a3af8
vince1887 <93718079+vince1887@users.noreply.github.com>
2023-11-30 16:01:14 -0500

    ajout du tir dans la scene 2

--- 524_T_ESCAPE_bolduc_desjardins/Assets/scripts/oscscript.cs
+++ 524_T_ESCAPE_bolduc_desjardins/Assets/scripts/oscscript.cs
@@ -7,9 +7,18 @@ public class oscscript : MonoBehaviour
 {
     public extOSC.OSCReceiver oscReceiver;
     public extOSC.OSCTransmitter oscTransmitter;
+
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 20f;
+
     Vector2 movement;
+    Vector2 mousePosition;
+
+    public Camera cam;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +29,30 @@ public class oscscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //deplacement code
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        //bullet code
+        if(Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
     }
 
     void mouvementPlayer(OSCMessage oscMessage) {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePosition - rb.position;
+        float angle = Mathf.Atan2(lookDir.y , lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+
+    void shoot() {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D>();
+        rbBullet.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
     }
 }
