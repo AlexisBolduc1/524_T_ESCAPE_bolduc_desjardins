e2a00db1b89444cccf8aa4f454ab7526ae223a56
vince1887 <93718079+vince1887@users.noreply.github.com>
2023-11-30 15:40:17 -0500

    player movement

--- 524_T_ESCAPE_bolduc_desjardins/Assets/playermovement.cs
+++ 524_T_ESCAPE_bolduc_desjardins/Assets/playermovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +6,15 @@ public class playermovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-
     Vector2 movement;
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
     }
 
     void FixedUpdate() {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-}
+}*/
--- 524_T_ESCAPE_bolduc_desjardins/Assets/scripts/oscscript.cs
+++ 524_T_ESCAPE_bolduc_desjardins/Assets/scripts/oscscript.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using extOSC;
 
 public class oscscript : MonoBehaviour
 {
+    public extOSC.OSCReceiver oscReceiver;
+    public extOSC.OSCTransmitter oscTransmitter;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        oscReceiver.Bind("/enc", mouvementPlayer);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void mouvementPlayer(OSCMessage oscMessage) {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
