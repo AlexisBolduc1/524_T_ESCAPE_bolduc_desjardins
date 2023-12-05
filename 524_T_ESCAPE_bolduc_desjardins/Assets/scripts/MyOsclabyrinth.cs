8eadf0a00370b6c935592af9a42eb74b6fc53901
vince1887 <vincentdesjardins1887@gmail.com>
2023-12-05 18:51:23 -0500

    convertire scene 1 avec osc

--- 524_T_ESCAPE_bolduc_desjardins/Assets/scripts/MyOsclabyrinth.cs
+++ 524_T_ESCAPE_bolduc_desjardins/Assets/scripts/MyOsclabyrinth.cs
@@ -8,50 +8,50 @@ public class MyOsclabyrinth : MonoBehaviour
     public extOSC.OSCTransmitter oscTransmitter;
     public GameObject player;
     float myChronoStart;
-    public float moveSpeed = 5f; // Add this line
-
-    public static float ScaleValue(float value, float inputMin, float inputMax, float outputMin, float outputMax)
-    {
-        return Mathf.Clamp(((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin), outputMin, outputMax);
-    }
-
-    void joueurCallback(extOSC.OSCMessage oscMessage)
-    {
-        if (oscMessage.Values.Count > 0 && oscMessage.Values[0].Type == extOSC.OSCValueType.Float)
-        {
-            float valeur = oscMessage.Values[0].FloatValue;
-
-            // Rotation and aiming
-            float hautbas = ScaleValue(valeur, 0, 4095, -2, 5);
-            player.transform.eulerAngles = new Vector3(0, hautbas, 0);
-
-            float dgjoueur = ScaleValue(valeur, 0, 4095, -12, 3);
-            player.transform.position = new Vector3(dgjoueur, 0, 0);
-        }
-    }
-
-void buttonCallback(extOSC.OSCMessage oscMessage)
-{
-    if (oscMessage.Values.Count > 0 && oscMessage.Values[0].Type == extOSC.OSCValueType.Int)
-    {
-        int buttonState = oscMessage.Values[0].IntValue;
-
-        // Forward movement when the button is pressed
-        if (buttonState == 0)
-        {
-            Debug.Log("Button Pressed. Moving forward!");
-
-            // Move the player forward along its own local forward direction (2D)
-            player.transform.Translate(Vector2.up * Time.deltaTime * moveSpeed);
-        }
-    }
-}
-
+    public float rotationSpeed = 1f;
+    public float moveSpeed = 5f;
+     private Rigidbody2D playerRigidbody2D;
+     private float accumulatedRotation = 0f;
 
     void Start()
     {
         oscReceiver.Bind("/enc", joueurCallback);
         oscReceiver.Bind("/button", buttonCallback);
+        playerRigidbody2D = player.GetComponent<Rigidbody2D>();
+    }
+
+
+void joueurCallback(extOSC.OSCMessage oscMessage)
+    {
+        if (oscMessage.Values.Count > 0 && oscMessage.Values[0].Type == extOSC.OSCValueType.Int)
+        {
+            int rotationInput = oscMessage.Values[0].IntValue;
+
+            // Normalize the rotation input to be between -1 and 1
+            float normalizedRotationInput = rotationInput / 4095f;
+
+            // Update the accumulated rotation
+            accumulatedRotation += normalizedRotationInput * rotationSpeed;
+
+            // Rotation and aiming using the accumulated rotation
+            player.transform.rotation = Quaternion.Euler(0, 0, accumulatedRotation);
+        }
+    }
+
+
+
+    void buttonCallback(extOSC.OSCMessage oscMessage)
+    {
+        if (oscMessage.Values.Count > 0 && oscMessage.Values[0].Type == extOSC.OSCValueType.Int)
+        {
+            int buttonState = oscMessage.Values[0].IntValue;
+
+            // Forward movement when the button is pressed
+            if (buttonState == 0)
+            {
+                player.transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+            }
+        }
     }
 
     void LateUpdate()
@@ -68,5 +68,10 @@ void buttonCallback(extOSC.OSCMessage oscMessage)
             myOscMessage.AddValue(extOSC.OSCValue.Int((int)myScaledPositionX));
             oscTransmitter.Send(myOscMessage);
         }
+    }
+
+    float ScaleValue(float value, float inputMin, float inputMax, float outputMin, float outputMax)
+    {
+        return Mathf.Clamp(((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin), outputMin, outputMax);
     }
 }
